f9ae341206fa2f46b0c11b07cb81e774a42a88a3
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-12-30 16:09:53 +0100

    centiseconds added

--- src/chronometer.js
+++ src/chronometer.js
@@ -21,6 +21,10 @@ class Chronometer {
     return this.currentTime % 60;
   }
 
+  getCentiseconds() {
+    return this.currentTime % 100; // Bonus Iteration 9: Centiseconds
+  }
+
   computeTwoDigitNumber(value) {
     return ("0" + value.toString()).slice(-2);
   }
@@ -34,6 +38,9 @@ class Chronometer {
   }
 
   split() {
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    const centiseconds = this.computeTwoDigitNumber(this.getCentiseconds());
     return `${this.getMinutes()}:${this.getSeconds()}`;
   }
 }
